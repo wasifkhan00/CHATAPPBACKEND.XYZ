010388f8ca23de775dc8924d3f1ae691e9c6fa2f
Wasif Khan <82050148+wasifkhan00@users.noreply.github.com>
2025-03-26 16:22:34 +0500

    Update Server.js
    https://snaptext.netlify.app/ origin added

--- Server.js
+++ Server.js
@@ -18,7 +18,7 @@ dotenv.config({ path: __dirname + "/.env" });
 const dbConnectionURI = process.env.DB_CONNECTION_STRING;//
 app.use(cors());
 app.use(cors({ origin: 'https://snaptext.netlify.app/' }));
-app.use(cors({ origin: "http://localhost:3000" }));
+// app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(auth);
 app.all("*", endpoints);
